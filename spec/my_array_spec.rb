85581aaa03daa32de4a77068dc12f564ce06a7c1
Randall Reed, Jr <randallreedjr@users.noreply.github.com>
2018-01-23 09:58:30 -0500

    Flatten on empty array returns array

--- spec/my_array_spec.rb
+++ spec/my_array_spec.rb
@@ -1,5 +1,15 @@
 RSpec.describe MyArray do
   describe '#flatten' do
+    context 'when array is empty' do
+      let(:array) { [] }
+
+      it 'retuns the empty array' do
+        my_array = MyArray.new(array)
+
+        expect(my_array.flatten).to eq []
+      end
+    end
+
     context 'when array is already flat' do
       let(:array) { [1,2,3,4] }
       it 'returns the array' do
